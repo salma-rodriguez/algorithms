f243a9f7d41748e5942b3ca9a5482589dc94a483
-- <-->
2013-02-02 22:25:05 -0500

    adding new functions

--- include/dequeue.h
+++ include/dequeue.h
@@ -1,8 +1,20 @@
+#ifndef __DEQUEUE_H
+#define __DEQUEUE_H
+
+int size;
+
 struct list_head *head;
 struct list_head *tail;
 
-void init(void *data);
-void list_pop_head();
-void list_pop_tail();
+void __free();
+void *list_pop_head();
+void *list_pop_tail();
 void list_push_head(void *data);
 void list_push_tail(void *data);
+
+#ifdef ALLOC
+static struct list_head *alloc(struct list_head *);
+#undef ALLOC
+#endif /* ALLOC */
+
+#endif /* __DEQUEUE_H */
