13d0c689ee15d1decee836facbbe4dc3ebd97a36
-- <-->
2013-02-19 17:38:57 -0500

    testing math library functions

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <fifo.h>
+#include <math.h>
 #include <array.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,10 @@
 #include <linked_list.h>
 
 #define BUFSIZE	1 << 10
+
+struct fifo *fifo;
+struct lifo *lifo;
+struct dequeue *dec;
 
 int compare(any_t t1, any_t t2)
 {
@@ -26,7 +31,7 @@ void test_search(int num)
 	int vals[BUFSIZE];
 	char buf[BUFSIZE];
 
-	list = create(list, compare);
+	list = create_array_list(list, compare);
 
 	i = n = 0;
 	while ((c = getchar()) != EOF) {
@@ -51,7 +56,7 @@ void test_search(int num)
 	printf("physical size of list: %d\n", list->size);
 	printf("number of items: %d\n", list->count);
 
-	list = destroy(list);
+	list = destroy_array_list(list);
 
 	printf(list? "not destroyed\n": "destroyed\n");
 }
@@ -62,34 +67,34 @@ void test_dequeue()
 	int arr[10];
 	struct list_head *p;
 
-	__free();
+	dec = create_dequeue(dec);
 	
 	for (i = 0; i < 10; i++)
 		arr[i] = i;
 
 	for (i = 0; i < 10; i++)
 		if (i%2) /* odd number */
-			list_push_tail(&arr[i]);
+			dec->push_tail(&arr[i], dec);
 		else	/* even number */
-			list_push_head(&arr[i]);
+			dec->push_head(&arr[i], dec);
 	
-	printf("value: %d\n", *(int *)head->data);
+	printf("value: %d\n", *(int *)dec->list->head->data);
 
-	list_for_each(p, head)
+	list_for_each(p, dec->list->head)
 		printf("value: %d\n", *(int *)p->data);
 
 	printf("\n");
 
-	printf("popped %d from head\n", *(int *)list_pop_head());
-	printf("popped %d from tail\n", *(int *)list_pop_tail());
+	printf("popped %d from head\n", *(int *)dec->pop_head(dec));
+	printf("popped %d from tail\n", *(int *)dec->pop_tail(dec));
 
 	printf("\n");
 
 	printf("new list in reverse:\n\n");
 
-	printf("value: %d\n", *(int *)tail->data);
+	printf("value: %d\n", *(int *)dec->list->tail->data);
 
-	list_for_each_reverse(p, tail)
+	list_for_each_reverse(p, dec->list->tail)
 		printf("value: %d\n", *(int *)p->data);
 }
 
@@ -99,37 +104,48 @@ void test_fifo()
 	int arr[10];
 	struct list_head *p;
 
-	__free();
+	fifo = create_fifo(fifo);
 	
 	for (i = 0; i < 10; i++)
 		arr[i] = i;
 
 	for (i = 0; i < 10; i++)
-		list_push_fifo(&arr[i]);
+		fifo->push(&arr[i], fifo);
 
-	printf("value: %d\n", *(int *)head->data);
+	printf("value: %d\n", *(int *)fifo->list->head->data);
 
-	list_for_each(p, head)
+	list_for_each(p, fifo->list->head)
 		printf("value: %d\n", *(int *)p->data);
 
 	printf("\n");
 
-	printf("popped %d from fifo\n", *(int *)list_pop_fifo());
-	printf("popped %d from fifo\n", *(int *)list_pop_fifo());
+	printf("popped %d from fifo\n", *(int *)fifo->pop(fifo));
+	printf("popped %d from fifo\n", *(int *)fifo->pop(fifo));
 
 	printf("\n");
 
 	printf("new list in reverse:\n\n");
 
-	printf("value: %d\n", *(int *)tail->data);
+	printf("value: %d\n", *(int *)fifo->list->tail->data);
 
-	list_for_each_reverse(p, tail)
+	list_for_each_reverse(p, fifo->list->tail)
 		printf("value: %d\n", *(int *)p->data);
+}
+
+void test_math()
+{
+	printf("6! is equal to %d\n", fac(6));
+	printf("the greatest common factor between 12 and 25 is %d\n", gcd(12, 25));
+	printf("the greatest common factor between 12 and 144 is %d\n", gcd(12, 144));
+	printf("the least common multiple between 3 and 4 is %d\n", lcm(3, 4));
+	printf("the least common multiple between 6 and 9 is %d\n", lcm(9, 6));
+	printf("the least common multiple between 12 and 144 is %d\n", lcm(12, 144));
 }
 
 void run_test(char *type)
 {
 	if (!strcmp(type, "fifo")) test_fifo();
+	if (!strcmp(type, "math")) test_math();
 	if (!strcmp(type, "dequeue")) test_dequeue();
 }
 
