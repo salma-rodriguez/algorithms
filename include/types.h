8fbf8aff857e88442f0f3427a0270160b6f307cb
-- <-->
2013-02-21 12:49:33 -0500

    adding common function types

---
+++ include/types.h
@@ -0,0 +1,29 @@
+typedef void * any_t;
+
+typedef int	(*__00)(void);
+typedef void	(*__01)(void);
+typedef any_t	(*__02)(void);
+
+typedef int	(*__10)(int);
+typedef void	(*__11)(int);
+typedef any_t	(*__12)(int);
+
+typedef int	(*__13)(any_t);
+typedef void	(*__14)(any_t);
+typedef any_t	(*__15)(any_t);
+
+typedef int	(*__20)(int, int);
+typedef void	(*__21)(int, int);
+typedef any_t	(*__22)(int, int);
+
+typedef int	(*__23)(int, any_t);
+typedef void	(*__24)(int, any_t);
+typedef any_t	(*__25)(int, any_t);
+
+typedef int	(*__26)(any_t, int);
+typedef void	(*__27)(any_t, int);
+typedef any_t	(*__28)(any_t, int);
+
+typedef int	(*__29)(any_t, any_t);
+typedef void	(*__2A)(any_t, any_t);
+typedef any_t	(*__2B)(any_t, any_t);
