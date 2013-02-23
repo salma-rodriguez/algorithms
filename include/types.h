8d97703218ef85e67dc6aac86f2bf00b1856a434
-- <-->
2013-02-23 00:02:11 -0500

    adding new function types

--- include/types.h
+++ include/types.h
@@ -27,3 +27,35 @@ typedef any_t	(*__28)(any_t, int);
 typedef int	(*__29)(any_t, any_t);
 typedef void	(*__2A)(any_t, any_t);
 typedef any_t	(*__2B)(any_t, any_t);
+
+typedef int	(*__30)(int, int, int);
+typedef void	(*__31)(int, int, int);
+typedef any_t	(*__32)(int, int, int);
+
+typedef int	(*__33)(int, int, any_t);
+typedef void	(*__34)(int, int, any_t);
+typedef any_t	(*__35)(int, int, any_t);
+
+typedef int	(*__36)(int, any_t, int);
+typedef void	(*__37)(int, any_t, int);
+typedef any_t	(*__38)(int, any_t, int);
+
+typedef int	(*__39)(int, any_t, any_t);
+typedef void	(*__3A)(int, any_t, any_t);
+typedef any_t	(*__3B)(int, any_t, any_t);
+
+typedef int	(*__3C)(any_t, int, int);
+typedef void	(*__3D)(any_t, int, int);
+typedef any_t	(*__3E)(any_t, int, int);
+
+typedef int	(*__3F)(any_t, int, any_t);
+typedef void	(*__3G)(any_t, int, any_t);
+typedef any_t	(*__3H)(any_t, int, any_t);
+
+typedef int	(*__3I)(any_t, any_t, int);
+typedef void	(*__3J)(any_t, any_t, int);
+typedef any_t	(*__3K)(any_t, any_t, int);
+
+typedef int	(*__3L)(any_t, any_t, any_t);
+typedef void	(*__3M)(any_t, any_t, any_t);
+typedef any_t	(*__3N)(any_t, any_t, any_t);
