fdde6a518c36fadddadaa261393ae2abdbceccef
-- <-->
2013-03-30 13:28:51 -0400

    rewriting isort

--- array/sort.c
+++ array/sort.c
@@ -11,7 +11,7 @@ void swap(any_t *a, any_t *b)
         *a = tmp;
 }
 
-struct array_list *isort(struct array_list *list)
+struct array_list *tsort(struct array_list *list)
 {
         int i, j;
         for (i = 1; i < list->get_count(list); i++)
@@ -22,12 +22,31 @@ struct array_list *isort(struct array_list *list)
         return list;
 }
 
+struct array_list *isort(struct array_list *list)
+{
+        int i, j;
+        any_t item;
+        for (j = 1; j < list->get_count(list); j++)
+        {
+                i = j-1;
+                item = list->lookup(j, list);
+                while (i >= 0 && list->compare(list->lookup(i, list), item) > 0)
+                {
+                        list->replace(i+1, list->lookup(i, list), list);
+                        i--;
+                }
+                list->replace(i+1, item, list);
+        }
+        return list;
+}
+
 struct array_list *msort(struct array_list * list)
 {
         int k;
         any_t middle;
-        struct array_list *left, *right;
+        struct array_list *left, *right, *both;
 
+        both = create_array_list(list->compare);
         left = create_array_list(list->compare);
         right = create_array_list(list->compare);
 
@@ -46,10 +65,10 @@ struct array_list *msort(struct array_list * list)
         left = msort(left);
         right = msort(right);
 
-        for (k = 0; k < list->get_count(list)/2; k++)
-                list->add_last(left->lookup(k, left), left);
-        for (k = 0; k < list->get_count(list)/2; k++)
-                list->add_last(right->lookup(k, right), right);
+        for (k = 0; k < left->get_count(left); k++)
+                both->add_last(left->lookup(k, left), both);
+        for (k = 0; k < right->get_count(right); k++)
+                both->add_last(right->lookup(k, right), both);
 
         free(left);
         free(right);
