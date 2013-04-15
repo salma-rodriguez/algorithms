af23f3b0ad373e92075fed2558c41ef07281a18b
salma-rodriguez <srodr063@fiu.edu>
2013-04-15 10:44:49 -0400

    merge sort now working

--- array/sort.c
+++ array/sort.c
@@ -2,6 +2,8 @@
 #include<array.h>
 #include<stdlib.h>
 
+#define INFINITY 0x7FFFFFFF
+
 void swap(any_t *a, any_t *b)
 {
         any_t tmp;
@@ -11,12 +13,15 @@ void swap(any_t *a, any_t *b)
         *a = tmp;
 }
 
+// Okay, I made this one up
+// Not a very efficient algorithm...
 struct array_list *tsort(struct array_list *list)
 {
         int i, j;
         for (i = 1; i < list->get_count(list); i++)
                 for (j = i; j >= 1; --j)
-                        if (list->compare(list->lookup(j, list), list->lookup(j-1, list)) < 0) {
+                        if (list->compare(list->lookup(j, list),
+                                                list->lookup(j-1, list)) < 0) {
                                 swap(&list->array[j], &list->array[j-1]);
                         }
         return list;
@@ -40,38 +45,56 @@ struct array_list *isort(struct array_list *list)
         return list;
 }
 
-struct array_list *msort(struct array_list * list)
+struct array_list *merge(struct array_list * list, int p, int q, int r)
 {
-        int k;
-        any_t middle;
-        struct array_list *left, *right, *both;
+        int i, j, k, n1, n2, t;
+        struct array_list *left, *right;
+        
+        n1 = q - p + 1;
+        n2 = r - q;
 
-        both = create_array_list(list->compare);
         left = create_array_list(list->compare);
         right = create_array_list(list->compare);
+                        
+        for (i = 0; i < n1; i++)
+                left->add_last(list->lookup(p + i, list), left);
 
-        if (list->get_count(list) < 30)
-                isort(list);
-       
-        middle = list->lookup(list->get_count(list)/2, list);
+        for (j = 0; j < n2; j++)
+                right->add_last(list->lookup(q + j + 1, list), right);
+        
+        t = INFINITY;
+        
+        left->add_last((void *)&t, left);
+        right->add_last((void *)&t, right);
 
-        for (k = 0; k < list->get_count(list); k++)
+        i = 0;
+        j = 0;
+
+        int cond1, cond2, cond3;
+        for (k = p; k <= r; k++)
         {
-                if (list->compare(middle, list->lookup(k, list)) < 0)
-                        left->add_last(list->lookup(k, list), left);
-                else right->add_last(list->lookup(k, list), right);
+                if (list->compare(left->lookup(i, left),
+                                        right->lookup(j, right)) <= 0)
+                        list->replace(k, left->lookup(i++, left), list);
+                else
+                        list->replace(k, right->lookup(j++, right), list);
         }
-
-        left = msort(left);
-        right = msort(right);
-
-        for (k = 0; k < left->get_count(left); k++)
-                both->add_last(left->lookup(k, left), both);
-        for (k = 0; k < right->get_count(right); k++)
-                both->add_last(right->lookup(k, right), both);
 
         free(left);
         free(right);
 
+        return list;
+}
+
+struct array_list *msort(struct array_list * list, int p, int r)
+{
+        int q;
+        if (p < r)
+        {
+                q = (p + r)/2;
+                list = msort(list, p, q);
+                list = msort(list, q + 1, r);
+                list = merge(list, p, q, r);
+        }
         return list;
 }
