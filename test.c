de9278d3371e88c102917d62f433e2615f4fd6fa
salma-rodriguez <srodr063@fiu.edu>
2013-04-15 19:46:14 -0400

    updating test file

--- test.c
+++ test.c
@@ -197,6 +197,7 @@ void test_math()
 	printf("the least common multiple between 6 and 9 is %d\n", lcm(9, 6));
 	printf("the least common multiple between 12 and 144 is %d\n", lcm(12, 144));
 	printf("2 to the power of 12 is %d\n", pwr(2, 12));
+	printf("3 to the power of 11 is %d\n", pwr(3, 11));
 	printf("the 46th Fibonacci number is: %d\n", fib(46));
 }
 
