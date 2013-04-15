6da50ea57506821afdb3293a01e974fcd648674c
salma-rodriguez <srodr063@fiu.edu>
2013-04-15 10:45:22 -0400

    making changes to msort function definition

--- include/sort.h
+++ include/sort.h
@@ -4,6 +4,6 @@
 #include <array.h>
 
 struct array_list *isort(struct array_list *);
-struct array_list *msort(struct array_list *);
+struct array_list *msort(struct array_list *, int p, int r);
 
 #endif /* __SORT_H__ */
