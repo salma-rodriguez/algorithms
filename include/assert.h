3f5bcf911692e0929f06b0004c5e42bc29221293
salma-rodriguez <srodr063@fiu.edu>
2013-04-15 10:52:09 -0400

    made changes to assert for no apparent reason

--- include/assert.h
+++ include/assert.h
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef __FLAG__
 #define ASSERT(x) if (!(x)) \
 	{ fprintf(stderr, "error!\n");  exit(0); }
+#else
+#define ASSERT(x)
+#endif
 
 #define ERROR(err, args...) \
 	fprintf(stderr, "ERROR: " err "\n", ##args); \
