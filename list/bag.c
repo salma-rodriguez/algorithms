c212331a843c0170929c70d427686fad43117fd1
salma-rodriguez <srodr063@fiu.edu>
2013-07-30 15:14:58 -0400

    changing function prototypes and a bunch of other updates

--- list/bag.c
+++ list/bag.c
@@ -1,56 +1,57 @@
 #include <bag.h>
 #include <assert.h>
 #include <dequeue.h>
+#include <linked_list.h>
 
 struct internal
 {
         struct list *list;
 };
 
-static any_t pop_faux(struct dequeue *);
-static void push_faux(any_t, struct dequeue *);
+static any_t peek_bag(any_t);
+static int get_size(any_t);
+static void push_bag(any_t, any_t);
 
-static struct any_t peek_bag(struct bag *bag);
-static struct void push_bag(any_t item, struct bag *bag);
-
-static struct bag *create_bag()
+struct bag *create_bag()
 {
         struct bag *bag;
 
 	bag = malloc(sizeof(struct bag));
 	bag->priv = malloc(sizeof(struct internal));
-        bag->priv->list = create_linked_list(
+        bag->priv->list = create_linked_list
                 (POOF_NONE | PEEK_HEAD | PUSH_HEAD);
 
-	bag->get_size = get_size;
 	bag->push = push_bag;
 	bag->peek = peek_bag;
+	bag->get_size = get_size;
+
 	return bag;
 }
 
-static void destroy_bag(struct bag *bag)
+void destroy_bag(struct bag *bag)
 {
 	destroy_linked_list(bag->priv->list);
 	free(bag->priv);
 	free(bag);
 }
 
-static void push_bag(any_t item, struct bag *bag)
+static int get_size(any_t obj)
 {
+        struct bag *bag;
+        bag = (struct bag *)obj;
+        return bag->priv->list->get_count(bag->priv->list);
+}
+
+static void push_bag(any_t item, any_t obj)
+{
+        struct bag *bag;
+        bag = (struct bag *)obj;
 	bag->priv->list->list_push_head(item, bag->priv->list);
 }
 
-static any_t peek_bag(struct bag *bag)
+static any_t peek_bag(any_t obj)
 {
-	return bag->priv->list->head->data;
-}
-
-static any_t pop_faux(struct dequeue *dec)
-{
-	ERROR("cannot pop from the head or tail of %p\n", dec);
-}
-
-static void push_faux(any_t item, struct dequeue *dec)
-{
-	ERROR("cannot push %p into the tail of %p\n", item, dec);
+        struct bag *bag;
+        bag = (struct bag *)obj;
+	return bag->priv->list->list_peek_head(bag->priv->list);
 }
