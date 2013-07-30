88f45f4c6d5227132968ab02752aa869c2cd6915
salma-rodriguez <srodr063@fiu.edu>
2013-07-30 14:46:50 -0400

    adding some boundary check to, changing msort to mxsort, and adding real merge sort

--- include/sort.h
+++ include/sort.h
@@ -4,6 +4,7 @@
 #include <array.h>
 
 struct array_list *isort(struct array_list *);
-struct array_list *msort(struct array_list *, int p, int r);
+struct array_list *mxsort(struct array_list *, int, int);
+struct array_list *msort(struct array_list *);
 
 #endif /* __SORT_H */
