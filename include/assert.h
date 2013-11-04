bc368f1b59dcdd5034f130c24de16a36f3334d2c
salma <syrodrig@us.ibm.com>
2013-11-03 21:28:47 -0500

    making it easier to print debug messages with ASSERTZ

--- include/assert.h
+++ include/assert.h
@@ -14,7 +14,16 @@
 #endif
 
 #define ERROR(err, args...) \
-	fprintf(stderr, "ERROR: " err "\n", ##args); \
-	exit(0);
+        { \
+                fprintf(stderr, "ERROR: " err "\n", ##args); \
+                exit(0); \
+        }
+
+#ifdef __FLAG__
+#define ASSERTZ(x, err, args...) if (!(x)) \
+        ERROR(err, ##args);
+#else
+#define ASSERTZ(x)
+#endif
 
 #endif /* __ASSERT_H */
