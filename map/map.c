c78d8dfbfdda6ecf32b483df0fac40e8749dc980
salma <syrodrig@us.ibm.com>
2013-11-30 23:11:09 -0500

    just moving things around and adding documentation

--- map/map.c
+++ map/map.c
@@ -9,18 +9,125 @@
 #define INSERT (1 << 1)
 #define DELETE (1 << 2)
 
-static void __alloc(struct map **);
-static void __priv_alloc(struct internal **);
-
-static inline int simple_hash(int);
-static comparable_t search(int, map_t);
-static comparable_t remove(int, map_t);
-static int insert(comparable_t, map_t);
+/*
+ * internal
+ */
 
 struct internal
 {                                                                       
         array_t array;
 };
+
+static u32  __lookup(int, comparable_t, map_t);
+static void __alloc(struct map **);
+static void __priv_alloc(struct internal **);
+
+/*
+ *
+ */
+
+/*
+ * Create the hash map data structure.
+ * @return map_t: the hash table created
+ */
+map_t create_hash_map();
+
+/*
+ * Destroy a hash map data structure.
+ * @parm1 map_t: the hash table to destroy
+ */
+void destroy_hash_map(map_t map);
+
+/*
+ * Search for an item in the hash map.
+ * @parm1 int: the unique identifier (key)
+ * @parm2 map_t: the hash map to search in
+ * @return comparable_t:
+ *      the item,       if found
+ *      ESEARCH,        if not found
+ */
+static comparable_t search(int, map_t);
+
+/*
+ * Insert a given item into the given table
+ * @parm1 comparable_t: the item to insert
+ * @parm2 map_t: the hash table
+ * @return int: 
+ *      the item,       if found
+ *      EINSERT,        if not found
+ */
+static int insert(comparable_t, map_t);
+
+/*
+ * Remove an item from the hash table with given key.
+ * @parm1 int: the unique identifier (key)
+ * @parm2 map_t: the hash table
+ * @return comparable_t:
+ *      the item,       if found
+ *      NULL,           if not found
+ */
+static comparable_t remove(int, map_t);
+
+/*
+ * Generic hash function
+ * @parm1 int: the unique identifier (key)
+ * @parm2 int: the probing index
+ * @return int: the value hashed to
+ */
+static inline int hash(int uid, int i);
+
+/*
+ * Map a given key to a fixed value.
+ *
+ * Note: this always results in collision.
+ *
+ * @return int: a constant hash value
+ */
+static inline int const_hash();
+
+/*
+ * Map a given key to a 1-1 value.
+ *
+ * Note: may result in collision
+ *       if input has non-uniform distribution
+ *
+ * @parm1 int: the unique identifier (key)
+ * @return the uid as hash value
+ */
+static inline int simple_hash(int);
+
+/*
+ * Generate low hash value for a given
+ * key (uid) and the given number of "rightmost" bits.
+ * @parm1 int: the unique identifier (key)
+ * @parm2 int: the number of "rightmost" bits
+ * @return int: the low x bits of uid as hash value
+ * */
+static inline int low_hash(int, int);
+
+/* 
+ * Generate mid square hash value for a given
+ * key (uid) and the given number of "middle" bits.
+ * @parm1 int: the unique identifier (key)
+ * @parm2 int: the number of "middle" bits
+ * @return int:
+ *      if uid = 0 | 1
+ *              uid itself
+ *      if x%2 = 0
+ *              the mid x bits of uid sq as hash value
+ *      if x%2 = 1
+ *              the mid x+1 bits of uid sq as hash value
+ * */
+static int midsq_hash(int, int);
+
+/*
+ * Generate the high hash value for a given
+ * key (uid) and the given number of "leftmost" bits.
+ * @parm1 int: the unique identifier (key)
+ * @parm2 int: the number of "leftmost" bits
+ * @return the high x bits of uid as hash value
+ * */
+static int high_hash(int uid, int x);
 
 map_t create_hash_map()
 {
@@ -43,9 +150,54 @@ void destroy_hash_map(map_t map)
         free(map);
 }
 
+static comparable_t search(int uid, map_t map)
+{
+        struct comparable obj = { (any_t)0, uid };
+        return (comparable_t)__lookup(SEARCH, &obj, map);
+}
+
+static int insert(comparable_t obj, map_t map)
+{
+        return (int)__lookup(INSERT, obj, map);
+}
+
+static comparable_t remove(int uid, map_t map)
+{
+        struct comparable obj = { (any_t)0, uid };
+        return (comparable_t)__lookup(DELETE, &obj, map);
+}
+
 static inline int hash(int uid, int i)
 {
         return simple_hash(uid);
+}
+
+static inline int const_hash()
+{
+        return CONST;
+}
+
+static inline int simple_hash(int uid)
+{
+        return uid;
+}
+                    
+static inline int low_hash(int uid, int x)
+{
+        return uid&((1<<x)-1);
+}
+
+static int midsq_hash(int uid, int x)
+{
+        int tr, sq;
+        sq = pwr(uid,2);
+        tr = (nobits(sq)-x)>>1;
+        return (sq&((1<<(nobits(sq)-tr))-1))>>tr;
+}
+
+static int high_hash(int uid, int x)
+{
+        return uid>>(nobits(uid)-x);
 }
 
 static u32 __lookup(int flag, comparable_t obj, map_t map)
@@ -76,9 +228,16 @@ loop:
 
                 if (flag == INSERT)
                 {
-                        ret = -1;
-                        goto fault;
+                        ret = -EINSERT;
+                        goto exit;
                 }
+
+        }
+
+        if ((comparable == NULL) & ((flag == DELETE) | (flag == SEARCH)))
+        {
+                ret = -ESEARCH;
+                goto exit;
         }
 
         switch (flag)
@@ -91,67 +250,13 @@ loop:
                         map->priv->array->add(key, obj, map->priv->array);
                         ret = (u32)key;
                         break;
+                case (SEARCH) :
+                        ret = (u32)comparable;
+                        break;
         }
 
-fault:
+exit:
         return (u32)ret;
-}
-
-static comparable_t search(int uid, map_t map)
-{
-        struct comparable obj = { (any_t)0, uid };
-
-        return (comparable_t)__lookup(SEARCH, &obj, map);
-}
-
-static int insert(comparable_t obj, map_t map)
-{
-        return (int)__lookup(INSERT, obj, map);
-}
-
-static comparable_t remove(int uid, map_t map)
-{
-        struct comparable obj = { (any_t)0, uid };
-        
-        return (comparable_t)__lookup(DELETE, &obj, map);
-}
-
-/** 
- * @return the uid as hash value
- * May result in collision if input has non-uniform distribution
- */
-static inline int simple_hash(int uid)
-{
-        return uid;
-}
-                    
-/**
- * @return a single value hashed from any uid
- * This always results in collision.
- */
-static inline int const_hash(int uid)
-{
-        return CONST;
-}
-
-/** @return the low x bits of uid as hash value */
-static inline int low_hash(unsigned int uid, int x)
-{
-        return uid&((1<<x)-1);
-}
-
-/** @return the high x bits of uid as hash value */
-static int high_hash(int uid, int x)
-{
-        return uid>>(nobits(uid)-x);
-}
-
-/** @return the mid x bits of uid sq as hash value */
-static int midsq_hash(int uid, int x)
-{
-        int sq;
-        sq = pwr(uid,2);
-        return (sq&((1<<(nobits(sq)-x))-1))>>x;
 }
 
 static void __alloc(struct map **map)
